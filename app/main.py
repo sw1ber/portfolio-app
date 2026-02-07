9b3abc2c7928fb84757a8f6a4bada9d3c89ceb9f
sw1ber <swiber37@naver.com>
2026-02-08 07:18:05 +0900

    commit

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
