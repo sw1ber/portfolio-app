2eceebc9eb791ac524a7f9ef8eedf57767bb7e21
sw1ber <swiber37@naver.com>
2026-02-08 07:08:33 +0900

    commit

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello FastAPI 👋"}
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "title": "My Portfolio"}
+    )
